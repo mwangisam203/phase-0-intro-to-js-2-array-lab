906dd7e307c81479caac05cc6dff68dabfbbfcf5
mwangisam203 <mwangisam203@gmail.com>
2023-09-29 13:26:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+console.log(cats); 
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  function appendCat(name) {
+    return cats.concat(name);
+  }
+  function prependCat(name) {
+    return [name].concat(cats);
+  }
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
